1c2b482ab15383c14ef60018f0ff3d323aaaea31
JoaquinGorosito <joaco.gorositoa@gmail.com>
2022-03-22 16:49:10 -0300

    Subiendo ejercicios unidad 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Unidad 1
+
 var fechaDeCumpleanios = 19;
 console.log(fechaDeCumpleanios);
 
@@ -9,4 +11,31 @@ console.log(anioDeNacimiento);
 
 var NumeroFavorito = 10;
 console.log(NumeroFavorito);
+
+
+// Unidad 2
+
+var cantidadDeVentanasEnMiCasa = 6 ;
+
+console.log(cantidadDeVentanasEnMiCasa);
+
+var soyHumano =  true;
+console.log(soyHumano);
+
+var miGustoDePizzaPreferido = 'Pepperoni';
+console.log("Mi gusto de pizza favorito es " + miGustoDePizzaPreferido);
+
+var mismaVariable = false;
+console.log(mismaVariable);
+
+mismaVariable = 'Hola';
+console.log(mismaVariable);
+
+
+mismaVariable = 0
+console.log(mismaVariable);
+
+
+mismaVariable = 'Minions'
+console.log(mismaVariable);
 
