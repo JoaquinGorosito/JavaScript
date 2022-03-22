b8dea6254c139a011bd40bda9d9cfe205f37b1ac
JoaquinGorosito <joaco.gorositoa@gmail.com>
2022-03-21 21:43:39 -0300

    subiendo practica 1

---
+++ index.js
@@ -0,0 +1,12 @@
+var fechaDeCumpleanios = 19;
+console.log(fechaDeCumpleanios);
+
+var mesCumpleanios = 10;
+console.log(mesCumpleanios);
+
+var anioDeNacimiento = 1989;
+console.log(anioDeNacimiento);
+
+var NumeroFavorito = 10;
+console.log(NumeroFavorito);
+
