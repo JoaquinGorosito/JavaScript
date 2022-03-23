c51893182bd7e3262c16b38e8a9a56db673ebb44
JoaquinGorosito <joaco.gorositoa@gmail.com>
2022-03-23 12:26:58 -0300

    Ejercicios practicos N 3

--- index.js
+++ index.js
@@ -39,3 +39,90 @@ console.log(mismaVariable);
 mismaVariable = 'Minions'
 console.log(mismaVariable);
 
+
+//Unidad 3
+
+//1)
+var noValgoNi5 = 4;
+console.log(noValgoNi5);
+
+
+//2)
+var miAnioDeNacimiento = 1989;
+//console.log(miAnioDeNacimiento);
+
+var meGustariaTener80Anios = 80;
+//console.log(meGustariaTener80Anios);
+
+var voyATener80ElAnio = (miAnioDeNacimiento + meGustariaTener80Anios);
+console.log(`En el año ${voyATener80ElAnio} cumplire 80 años`);
+
+
+//3)
+var costoDeUnaTele = 10000;
+
+var ahorros = 9000;
+
+var dineroQueMeFalta = (costoDeUnaTele - ahorros);
+console.log(`El dinero que me falta es ${dineroQueMeFalta}`);
+
+//4)
+var diasPorAnio = 365;
+
+var cantidadDeAniosEnUnaDecada = 10;
+
+var cantidadDeDiasEnUnaDecada = (diasPorAnio * cantidadDeAniosEnUnaDecada)
+
+console.log(`La cantidad de dias en 10 años son ${cantidadDeDiasEnUnaDecada}`);
+
+//5)
+var cantidadDePorciones = (24 / 8);
+console.log(`La cantidad de pizzas que tengo son ${cantidadDePorciones}`);
+
+//6)
+var  miAnimalFavorito = 'Perro';
+
+var sonIguales = 'Perro' == miAnimalFavorito;
+
+console.log(sonIguales);
+
+//7)
+var flan = 'flan';
+
+var flanDos = 'flan con dulce de leche'
+
+console.log((flan != flanDos));
+
+//8)
+var soyMayorDeEdad = 32 > 17;
+
+console.log(soyMayorDeEdad);
+
+//9)
+var soyMenorDe25 = 32 < 25;
+
+console.log(soyMenorDe25);
+
+//10)
+var estamosEnEnero = ('Marzo' === 'Enero');
+
+console.log(estamosEnEnero);
+
+//11)
+var noEstamosEnEnero = ('Marzo' !== 'Enero');
+
+console.log(noEstamosEnEnero);
+
+//12)
+var miNotaEnElParcial = 8;
+
+var notaMinimaParaAprobar = 6;
+
+var estoyAprobado = (miNotaEnElParcial >= notaMinimaParaAprobar);
+
+console.log(estoyAprobado);
+
+//13)
+var esHoraDeAlmorzar = 13 <= 12;
+
+console.log(esHoraDeAlmorzar);
