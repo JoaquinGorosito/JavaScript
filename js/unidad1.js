2ac5b8bfe321fcf0d117da516751d69b9f5ca9f0
JoaquinGorosito <joaco.gorositoa@gmail.com>
2022-03-26 12:08:25 -0300

    Ordenando archivos

---
+++ js/unidad1.js
@@ -0,0 +1,13 @@
+//Unidad 1
+
+var fechaDeCumpleanios = 19;
+console.log(fechaDeCumpleanios);
+
+var mesCumpleanios = 10;
+console.log(mesCumpleanios);
+
+var anioDeNacimiento = 1989;
+console.log(anioDeNacimiento);
+
+var NumeroFavorito = 10;
+console.log(NumeroFavorito);
---
+++ js/unidad2.js
@@ -0,0 +1,25 @@
+// Unidad 2
+
+var cantidadDeVentanasEnMiCasa = 6 ;
+
+console.log(cantidadDeVentanasEnMiCasa);
+
+var soyHumano =  true;
+console.log(soyHumano);
+
+var miGustoDePizzaPreferido = 'Pepperoni';
+console.log("Mi gusto de pizza favorito es " + miGustoDePizzaPreferido);
+
+var mismaVariable = false;
+console.log(mismaVariable);
+
+mismaVariable = 'Hola';
+console.log(mismaVariable);
+
+
+mismaVariable = 0
+console.log(mismaVariable);
+
+
+mismaVariable = 'Minions'
+console.log(mismaVariable);
